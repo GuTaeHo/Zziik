8b3f6a5f983758cda0a9061f7bc5fb26ecf221fe
GuTaeho <sbpdt1062@gmail.com>
2025-01-09 00:16:21 +0900

    Fix: KeychainManager append 오류 수정

--- Zziik/Sources/AppDelegate.swift
+++ Zziik/Sources/AppDelegate.swift
@@ -12,9 +12,20 @@ import KakaoSDKCommon
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidFinishLaunching(_ application: UIApplication) {
         initKakaoSDK()
+        initKeychain()
     }
     
     private func initKakaoSDK() {
         KakaoSDK.initSDK(appKey: InfoPlistManager.shared.kakaoNativeAppKey)
+    }
+    
+    private func initKeychain() {
+        if (try? KeychainManager.shared.fetch(.kakaoNativeKey, type: String.self)) == nil {
+            do {
+                try KeychainManager.shared.append(.kakaoNativeKey, value: InfoPlistManager.shared.kakaoNativeAppKey)
+            } catch (let error) {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
--- Zziik/Sources/Support/Manager/KeychainManager.swift
+++ Zziik/Sources/Support/Manager/KeychainManager.swift
@@ -9,8 +9,8 @@ import Foundation
 
 
 enum KeychainError: LocalizedError {
-    case appendFail
-    case fetchFail
+    case appendFail(reason: String)
+    case fetchFail(reason: String)
 }
 
 class KeychainManager {
@@ -24,7 +24,7 @@ class KeychainManager {
         
         var secClass: CFString {
             switch self {
-            case .kakaoNativeKey: return kSecClassKey
+            case .kakaoNativeKey: return kSecClassGenericPassword
             }
         }
     }
@@ -32,13 +32,18 @@ class KeychainManager {
     func append<T: Codable>(_ key: Key, value: T) throws {
         let data = try JSONEncoder().encode(value)
         
-        let query: [CFString: Any] = [kSecClass: key.secClass,
-                                      kSecAttrService: tag,
-                                      kSecAttrAccount: key.rawValue,
-                                      kSecValueData: data]
+        let query: [String: Any] = [kSecClass as String: key.secClass,
+                                      kSecAttrService as String: tag,
+                                      kSecAttrAccount as String: key.rawValue,
+                                      kSecValueData as String: data]
         
-        if SecItemAdd(query as CFDictionary, nil) != errSecSuccess {
-            throw KeychainError.appendFail
+        let status = SecItemAdd(query as CFDictionary, nil)
+        if status != errSecSuccess {
+            if let error: String = SecCopyErrorMessageString(status, nil) as String? {
+                throw KeychainError.appendFail(reason: error)
+            } else {
+                throw KeychainError.appendFail(reason: "알 수 없음")
+            }
         }
     }
     
@@ -51,7 +56,7 @@ class KeychainManager {
     }
     
     /// **key** 와 일치하는 모든 데이터를 배열로 반환
-    func fetch<T: Codable>(_ key: Key) throws -> [T] {
+    func fetch<T: Codable>(_ key: Key, type: T.Type) throws -> [T] {
         let query: [CFString: Any] = [kSecClass: key.secClass,
                                       kSecAttrService: tag,
                                       kSecAttrAccount: key.rawValue,
@@ -62,10 +67,19 @@ class KeychainManager {
         let status = SecItemCopyMatching(query as CFDictionary, &item)
         
         if status == errSecSuccess, let data = item as? Data {
-            return try JSONDecoder().decode([T].self, from: data)
+            do {
+                let jsonData = try JSONDecoder().decode([T].self, from: data)
+                return jsonData
+            } catch (let error) {
+                throw error
+            }
+        } else {
+            if let error: String = SecCopyErrorMessageString(status, nil) as String? {
+                throw KeychainError.fetchFail(reason: error)
+            } else {
+                throw KeychainError.fetchFail(reason: "알 수 없음")
+            }
         }
-        
-        throw KeychainError.fetchFail
     }
     
     private init() {
